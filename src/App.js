b886964131a03075a54e8866094ff5b784c0ffc2
RMGM2019 <48869312+RMGM2019@users.noreply.github.com>
2024-09-23 17:04:40 -0600

    Second 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Second Attempt 1
+          Second Attempt 2
         </p>
         <a
           className="App-link"
